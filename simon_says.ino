0a65245e13dac651545838afec8ff8ae20d840a0
Fabian144 <fabianyousif24@gmail.com>
2026-09-17 20:26:26 +0200

    .

--- simon_says.ino
+++ simon_says.ino
@@ -383,12 +383,10 @@ void audio_info(const char *info) {
   Serial.print("info ");
   Serial.println(info);
 }
-
 void audio_id3data(const char *info) { //id3 metadata
   Serial.print("id3data ");
   Serial.println(info);
 }
-
 void audio_eof_mp3(const char *info) { //end of file
   Serial.print("eof_mp3 ");
   Serial.println(info);
